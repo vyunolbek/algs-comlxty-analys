e880e1f461c4c31aff312409bf9de389fa3b36e2
vyunolbek <danila.vyunov@visrobo.com>
2023-10-26 16:19:41 +0300

    added k-merge sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import structures.dheap as dh
 from random import randint
-from structures import k_mergesort
+from structures.k_mergesort import KMergeSort
 
 dheap = dh.DHeap(10)
 arr = [12, 12, 13, 5, 6, 7]
@@ -9,6 +9,8 @@ sorted_arr = dheap.heap_sort(arr)
 arr = [12, 11, 13, 5, 6, 7, 1, 3, 8, 2, 9, 4]
 print(sorted_arr)
 
-# arr = [randint(0, 100) for i in range(10)]
+arr = [randint(0, 100) for i in range(10)]
 
-# print(k_mergesort.k_merge_sort(arr, 4))
+k_mergesort = KMergeSort(4)
+
+print(k_mergesort.k_merge_sort(arr))
--- structures/k_mergesort.py
+++ structures/k_mergesort.py
@@ -1,34 +1,38 @@
-def k_merge_sort(arr, k):
-    if len(arr) <= 1:
-        return arr
-    else:
-        n = len(arr)
-        chunk_size = n // k if n // k > 0 else 1
-        chunks = [arr[i:i+chunk_size] for i in range(0, n, chunk_size)]
-        sorted_chunks = [k_merge_sort(chunk, k) for chunk in chunks]
-        return merge(sorted_chunks)
+class KMergeSort:
 
-def merge(sorted_chunks):
-    if len(sorted_chunks) == 1:
-        return sorted_chunks[0]
-    else:
-        mid = len(sorted_chunks) // 2
-        left = merge(sorted_chunks[:mid])
-        right = merge(sorted_chunks[mid:])
-        return merge_two_lists(left, right)
+    def __init__(self, k):
+        self.k = k
 
-def merge_two_lists(left, right):
-    result = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
+    def k_merge_sort(self, arr, k=None):
+        if len(arr) <= 1:
+            return arr
         else:
-            result.append(right[j])
-            j += 1
-    result += left[i:]
-    result += right[j:]
-    return result
+            n = len(arr)
+            chunk_size = n // self.k if n // self.k > 0 else 1
+            chunks = [arr[i:i+chunk_size] for i in range(0, n, chunk_size)]
+            sorted_chunks = [self.k_merge_sort(chunk, self.k) for chunk in chunks]
+            print(arr)
+            return self.merge(sorted_chunks)
 
-a = []
+    def merge(self, sorted_chunks):
+        if len(sorted_chunks) == 1:
+            return sorted_chunks[0]
+        else:
+            mid = len(sorted_chunks) // 2
+            left = self.merge(sorted_chunks[:mid])
+            right = self.merge(sorted_chunks[mid:])
+            return self.merge_two_lists(left, right)
+
+    def merge_two_lists(self, left, right):
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result += left[i:]
+        result += right[j:]
+        return result
